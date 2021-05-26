1f82bd47d3744cc3307fb180e18ef780e417b342
Oyeleke Ayomide <haywhizzz@gmail.com>
2021-05-26 09:17:48 +0100

    Add app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,35 @@
+import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
-const App = () => (
-  <>
-    <h1>Calculator</h1>
-    <Display result="0" />
-    <ButtonPanel />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    this.setState(
+      state => calculate(state, buttonName),
+    );
+  }
+
+  render() {
+    const { total, next, operation } = this.state;
+    return (
+      <>
+        <h1>Calculator</h1>
+        <Display result={total || next || '0'} next={next} operation={operation} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </>
+    );
+  }
+}
 
 export default App;
