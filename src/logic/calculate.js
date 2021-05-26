82b44fccca22c0b0f77e6730240ae742720ff13e
Oyeleke Ayomide <haywhizzz@gmail.com>
2021-05-26 15:08:04 +0100

    Assign arrow function

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -2,7 +2,7 @@ import operate from './operate';
 
 let operationTotal = null;
 
-export default (calculator, buttonName) => {
+const calculate = (calculator, buttonName) => {
   const operations = ['+', '-', 'x', '÷', '%'];
   const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
@@ -74,3 +74,4 @@ export default (calculator, buttonName) => {
 
   return { total, next, operation };
 };
+export default calculate;
