f9280a5319324f400d201d52901f6a37f557c091
Oyeleke Ayomide <haywhizzz@gmail.com>
2021-05-26 14:04:21 +0100

    Correct changes suggested by TSE

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,31 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = (props) => {
-  const { name } = props;
-  const { clickHandler } = props;
+const Button = props => {
+  const { name, handleClick } = props;
   return (
-    <button type="button" onClick={() => clickHandler(name)}>{name}</button>
+    <>
+      <button
+        type="button"
+        onClick={() => {
+          handleClick(name);
+        }}
+        value={name}
+      >
+        {name}
+      </button>
+    </>
   );
 };
 
 Button.propTypes = {
-  name: PropTypes.string.isRequired,
-  clickHandler: PropTypes.func.isRequired,
+  name: PropTypes.string,
+  handleClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  name: '',
+  handleClick: undefined,
 };
 
 export default Button;
