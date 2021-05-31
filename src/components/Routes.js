ffe55eccd9658dc15c03daca0cd64f991836e265
Oyeleke Ayomide <haywhizzz@gmail.com>
2021-05-31 16:54:54 +0100

    fix eslint errors

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import App from '../calculator/App';
-import Nav from '../navbar/Navbar';
-import Home from '../home/Home';
-import Quote from '../quote/Quote';
+import App from './calculator/App';
+import Nav from './navbar/Navbar';
+import Home from './home/Home';
+import Quote from './quote/Quote';
 
 const Routes = () => (
   <BrowserRouter>
