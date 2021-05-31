3d8ecaa72c64ff9a40c01838f7754b82a189ce57
Oyeleke Ayomide <haywhizzz@gmail.com>
2021-05-31 08:57:05 +0100

    Add quote page

---
+++ src/components/Quote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Quote = () => (
+  <div id="quote">
+    <h1>Quote</h1>
+    <p>
+    "Pure mathematics is, in its way, the poetry of logical ideas."
+        — Albert Einstein, German theoretical physicist
+    </p>
+  </div>
+);
+
+export default Quote;
